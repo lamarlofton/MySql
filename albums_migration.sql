06d143156c6f16ae7ed45082a9c6f3df8dc9fd93
La Mar Lofton <lamarlofton55@gmail.com>
2018-08-10 16:29:10 -0500

    made my first table in mysql

---
+++ albums_migration.sql
@@ -0,0 +1,16 @@
+
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(255) NOT NULL,
+name VARCHAR (255) NOT NULL,
+release_date INT UNSIGNED NOT NULL,
+sales FLOAT,
+genre VARCHAR (255),
+PRIMARY KEY (id)
+-- primary KEY(id) -- instead, we'll add it to the column definition
+);
+
+DESCRIBE albums
