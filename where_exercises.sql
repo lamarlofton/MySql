157821be16d69db75afb834a4e0959a50ac19a8b
La Mar Lofton <lamarlofton55@gmail.com>
2018-08-14 09:32:42 -0500

    learning and using the where clauses

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,3 +3,48 @@ USE employees;
 select *
 from employees
 where first_name in ('Irena', 'Vidya', 'Maya');
+
+
+select *
+from employees
+where last_name like 'E%';
+
+select *
+from employees
+where hire_date between '1990-01-01' and '1999-12-31';
+
+
+select *
+from employees
+where birth_date like '%-12-25%';
+
+select *
+from employees
+where last_name like '%q%';
+
+select * from employees where first_name in ('Irena','Vidya', 'Maya')
+and gender = 'M';
+
+
+select *
+from employees
+where last_name like '%e'
+or last_name like 'e%';
+
+select *
+from employees
+where last_name like '%e'
+and last_name like 'e%';
+
+select *
+from employees
+where hire_date
+like '199%'
+and birth_date like '%12-25';
+
+
+select *
+from employees
+where last_name like '%q%'
+and
+last_name not like '%qu%';
